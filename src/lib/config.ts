f5ef539316b1116764bebec88e1aa65bce7af570
Brian Flad <bflad417@gmail.com>
2024-11-25 10:55:09 -0500

    Add testing

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -81,7 +81,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0.0",
-  sdkVersion: "0.2.0",
+  sdkVersion: "0.3.1",
   genVersion: "2.461.4",
-  userAgent: "speakeasy-sdk/typescript 0.2.0 2.461.4 1.0.0 petstore",
+  userAgent: "speakeasy-sdk/typescript 0.3.1 2.461.4 1.0.0 petstore",
 } as const;
