21c2dac2776589b698d9bcf0e081f8c03584f81d
Brian Flad <bflad417@gmail.com>
2024-11-26 12:54:45 -0500

    Upgrade Speakeasy to latest

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -86,7 +86,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0.0",
-  sdkVersion: "0.3.2",
+  sdkVersion: "0.3.3",
   genVersion: "2.461.4",
-  userAgent: "speakeasy-sdk/typescript 0.3.2 2.461.4 1.0.0 petstore",
+  userAgent: "speakeasy-sdk/typescript 0.3.3 2.461.4 1.0.0 petstore",
 } as const;
